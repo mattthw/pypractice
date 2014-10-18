22626f36a8a7e8ba1fa7f3e68ff4e452fc8dd1cb
Matt <mattmccoy94@gmail.com>
2014-10-17 22:27:54 -0400

    fixed bad input for main menu if input was not a digit. Not planning to fix for submenus. I only fixed this for the menu because I would sometimes push 1 for input and type it in the same string.
    I also added the option to remove a string

--- strstore.py
+++ strstore.py
@@ -3,8 +3,6 @@
 import sys
 import os
 
-data = []
-
 class Storage(object):
 
     def store(self, *args):
@@ -15,13 +13,17 @@ class Storage(object):
         global data
         return data
 
+    def remove_str(self, *args):
+        global data
+        data.remove(data[int(args[0])])
+
 class Dialogue(object):
 
     def welcome(self):
         os.system('cls' if os.name == 'nt' else 'clear')
         print 'Hello. Plase select an option.'
-        message = {1: 'add new text',2: 'view text',3: 'exit'}
-        for x in range(1,4):
+        message = {1: 'add new text',2: 'view text',3: 'remove text',4: 'exit'}
+        for x in range(1,5):
             print repr(x).rjust(2), 'to %s.'.rjust(3) % (message[x])
 
         return raw_input(': ')
@@ -42,20 +44,47 @@ class Dialogue(object):
         print 'You may type your string in below'
         return raw_input(': ')
 
+    def remove_text(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print'What string would you like to remove?'
+        global data
+        for x in range(len(data)):
+            print '|'.rjust(1), repr(x).rjust(2), data[x].rjust(3)
+        print 'Type the number of the string OR press ENTER to return.'
+        return raw_input(': ')
+        if raw_input(': ') == str():
+            main()
+        else:
+            return int(raw_input(': '))
+            main()
+
+
+#        if raw_input(': ') == str(1):
+ #           main()
+  #      else:
+   #         exit()
+
+#GLOBAL
+data = []
+
 def _return():
     main()
 
 def main():
     selec = Dialogue().welcome()
-    if int(selec) == '':
+    if selec.isdigit() == 0:
         _return()
-
+    if selec == '':
+        _return()
     if int(selec) == 1: #store new string
         Storage().store(Dialogue().new_string())
         main() #return to menu
     elif int(selec) == 2: #view stored string
         # print Storage().retrieve()
         Dialogue().list()
+    elif int(selec) == 3: #remove string
+        Storage().remove_str(Dialogue().remove_text())
+        main()
     else: #exit on bad response or quit
         sys.exit()
 
